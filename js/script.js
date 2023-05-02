a3277c2be371776283291a7e2f171c11b3690ea3
shahrokh-t <tahmoores54@gmail.com>
2023-05-02 16:07:02 +1000

    add details page backdrop

--- js/script.js
+++ js/script.js
@@ -63,6 +63,10 @@ async function displayMovieDetails() {
 
     const movie = await fetchAPIData(`movie/${movieId}`);
 
+    // Overlay for background image
+
+    displayBackgroundImage("movie", movie.backdrop_path);
+
     const div = document.createElement("div");
     div.innerHTML = `
     <div class="details-top">
@@ -106,6 +110,31 @@ async function displayMovieDetails() {
     document.querySelector("#movie-details").appendChild(div)
 
 }
+
+// Display backdrop on details pages
+function displayBackgroundImage(type, backgroundPath) {
+  const overlayDiv = document.createElement("div");
+  overlayDiv.style.backgroundImage = `url(https://image.tmdb.org/t/p/original/${backgroundPath})`;
+  overlayDiv.style.backgroundSize = "cover";
+  overlayDiv.style.backgroundPosition = "center";
+  overlayDiv.style.backgroundRepeat = "no-repeat";
+  overlayDiv.style.height = "100vh";
+  overlayDiv.style.width = "100vw";
+  overlayDiv.style.position = "absolute";
+  overlayDiv.style.top = "0";
+  overlayDiv.style.left = "0";
+  overlayDiv.style.zIndex = "-1";
+  overlayDiv.style.opacity = "0.1";
+
+  if(type === "movie") {
+    document.querySelector("#movie-details").appendChild(overlayDiv)
+  } else {
+    document.querySelector("#show-details").appendChild(overlayDiv)
+
+  }
+
+}
+
 // Fetch data from TMDB API
 async function fetchAPIData(endpoint) {
     const API_KEY = "2dc5d9e4e2fa9860d47c19237f303c55";
