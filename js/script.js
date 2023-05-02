f4843c88644dbf460e41a667171685c8a66e4ab7
shahrokh-t <tahmoores54@gmail.com>
2023-05-02 15:42:05 +1000

    add movie details

--- js/script.js
+++ js/script.js
@@ -11,16 +11,10 @@ async function displayPopularMovies() {
         div.innerHTML = `
         <a href="movie-details.html?id=${movie.id}">
         ${movie.poster_path
-                ? `<img
-            src="https://image.tmdb.org/t/p/w500${movie.poster_path}"
-            class="card-img-top"
-            alt="${movie.title}"
-          />`
-                : `<img
-          src="images/no-image.jpg"
-          class="card-img-top"
-          alt="Movie Title"
-        />`
+                ?
+                `<img src="https://image.tmdb.org/t/p/w500${movie.poster_path}" class="card-img-top" alt="${movie.title}" />`
+                :
+                `<img src="images/no-image.jpg" class="card-img-top" alt="Movie Title" />`
             }
         </a>
         <div class="card-body">
@@ -44,16 +38,11 @@ async function displayPopularShows() {
         div.innerHTML = `
         <a href="tv-details.html?id=${show.id}">
         ${show.poster_path
-                ? `<img
-            src="https://image.tmdb.org/t/p/w500${show.poster_path}"
-            class="card-img-top"
-            alt="${show.name}"
-          />`
-                : `<img
-          src="images/no-image.jpg"
-          class="card-img-top"
-          alt="Show Title"
-        />`
+                ?
+                `<img src="https://image.tmdb.org/t/p/w500${show.poster_path}"
+            class="card-img-top" alt="${show.name}" />`
+                :
+                `<img src="images/no-image.jpg" class="card-img-top" alt="Show Title" />`
             }
         </a>
         <div class="card-body">
@@ -68,7 +57,55 @@ async function displayPopularShows() {
     });
 };
 
+// Display movie details
+async function displayMovieDetails() {
+    const movieId = window.location.search.split("=")[1];
 
+    const movie = await fetchAPIData(`movie/${movieId}`);
+
+    const div = document.createElement("div");
+    div.innerHTML = `
+    <div class="details-top">
+    <div>
+    ${movie.poster_path
+            ?
+            `<img src="https://image.tmdb.org/t/p/w500${movie.poster_path}" class="card-img-top" alt="${movie.name}"/>`
+            :
+            `<img src="images/no-image.jpg" class="card-img-top" alt="Show Title"/>`
+        }
+    </div>
+    <div>
+      <h2>${movie.title}</h2>
+      <p>
+        <i class="fas fa-star text-primary"></i>
+        ${movie.vote_average.toFixed(1)} / 10
+      </p>
+      <p class="text-muted">Release Date: ${movie.release_date}</p>
+      <p>
+        ${movie.overview}
+      </p>
+      <h5>Genres</h5>
+      <ul class="list-group">
+        ${movie.genres.map((genre) => `<li>${genre.name}</li>`).join("")}
+      </ul>
+      <a href="${movie.homepage}" target="_blank" class="btn">Visit Movie Homepage</a>
+    </div>
+  </div>
+  <div class="details-bottom">
+    <h2>Movie Info</h2>
+    <ul>
+      <li><span class="text-secondary">Budget:</span> $${addCommasToNumber(movie.budget)}</li>
+      <li><span class="text-secondary">Revenue:</span> $${addCommasToNumber(movie.revenue)}</li>
+      <li><span class="text-secondary">Runtime:</span> ${movie.runtime} min</li>
+      <li><span class="text-secondary">Status:</span> ${movie.status}</li>
+    </ul>
+    <h4>Production Companies</h4>
+    <div class="list-group">${movie.production_companies.map(company => `<span>${company.name}</span>`).join(", ")}</div>
+  </div>
+    `;
+    document.querySelector("#movie-details").appendChild(div)
+
+}
 // Fetch data from TMDB API
 async function fetchAPIData(endpoint) {
     const API_KEY = "2dc5d9e4e2fa9860d47c19237f303c55";
@@ -107,6 +144,10 @@ function highlightActiveLink() {
     });
 }
 
+function addCommasToNumber (number) {
+  return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+}
+
 // Init app
 function init() {
     switch (global.currentPage) {
@@ -118,7 +159,7 @@ function init() {
             displayPopularShows();
             break;
         case "/movie-details.html":
-            console.log("Moive Details");
+            displayMovieDetails();
             break;
         case "/tv-details.html":
             console.log("TV Details");
